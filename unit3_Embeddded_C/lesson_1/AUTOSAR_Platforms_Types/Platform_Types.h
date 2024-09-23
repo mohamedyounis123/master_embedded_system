6b09f8588f618ede0e9ceae772e5cdbfa5a980c6
mohamed younis <mohamed2004younis@gmail.com>
2024-09-23 17:43:42 +0400

    AUTOSAR_Platforms_TypesAUTOSAR_Platforms_Types

---
+++ unit3_Embeddded_C/lesson_1/AUTOSAR_Platforms_Types/Platform_Types.h
@@ -0,0 +1,25 @@
+
+
+#ifndef PLATFORM_TYPES_H_
+#define PLATFORM_TYPES_H_
+
+typedef unsigned char   boolean;
+typedef unsigned char   uint8;
+typedef signed char     sint8;
+typedef unsigned short  unint16;
+typedef signed short    sint16;
+typedef unsigned int    uint32;
+typedef signed int      sint32;
+
+typedef float  float32;
+typedef double double64;
+
+typedef volatile signed char     vint8;
+typedef volatile unsigned char   vuint8;
+typedef volatile signed short    vint16;
+typedef volatile unsigned short  vuint16;
+typedef volatile signed int      vint32;
+typedef volatile unsigned int    vuint32;
+
+
+#endif 
